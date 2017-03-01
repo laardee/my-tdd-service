e438ac768a74f122903245ad163ac7b148f48507
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-03-01 06:15:55 +0200

    fix regex, add link to blog post, move time handler to time directory

--- test/currentDate.js
+++ test/currentDate.js
@@ -21,7 +21,7 @@ describe('currentDate', () => {
     return wrapped.run({}).then((response) => {
       const body = JSON.parse(response.body);
       expect(response.statusCode).to.be.equal(200);
-      expect(body.message).to.match(/.*\s(Sun|Mon|Tue|Web|Thu|Fri|Sat)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Nov|Dec)\s[0-3]\d{1}\s\d{4}\./);
+      expect(body.message).to.match(/.*\s(Sun|Mon|Tue|Wed|Thu|Fri|Sat)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s[0-3]\d{1}\s\d{4}\./);
     });
   });
 });
